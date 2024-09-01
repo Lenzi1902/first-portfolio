0820b57414b704e19598a69a7c993168e75f0bfc
Lenzi1902 <akungithub158@gmail.com>
2024-09-01 22:25:39 +0800

    update about component

--- app/component/about/About.tsx
+++ app/component/about/About.tsx
@@ -17,28 +17,20 @@ const About = () => {
     <div className=" h-screen w-screen text-black px-[119px] py-[52px]">
       <div className=" h-full w-full grid grid-cols-10 grid-rows-12 gap-4">
         <div className="bg-[#232323] rounded-xl col-span-3 row-span-8 p-[30px] flex flex-col justify-between relative group ">
-          <video autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover rounded-xl opacity-0 group-hover:opacity-[1] transition-all duration-1000">
-            <source src="./wavelinebackground.mp4" />
-          </video>
+          <video src="./wavelinebackground.mp4" autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover rounded-xl opacity-0 group-hover:opacity-[1] transition-all duration-1000" />
           <div className=" z-[1]">
             <img src="./fotoprofileportfolio.svg" alt="" className=" w-[85px] h-[85px]" />
             <p className=" font-[eczar] text-white font-bold text-[25px]">My name is Agung, I’m a Fullstack Web Developer and living in Indonesia</p>
           </div>
         </div>
         <div className="bg-[#232323] rounded-xl col-span-2 row-span-4 group relative">
-          <video autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover rounded-xl opacity-0 group-hover:opacity-[1] transition-all duration-1000">
-            <source src="./wavelinebackground.mp4" />
-          </video>
+          <video src="./wavelinebackground.mp4" autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover rounded-xl opacity-0 group-hover:opacity-[1] transition-all duration-1000" />
         </div>
         <div className="bg-[#232323] rounded-xl col-span-2 row-span-4 group relative">
-          <video autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover rounded-xl opacity-0 group-hover:opacity-[1] transition-all duration-1000">
-            <source src="./wavelinebackground.mp4" />
-          </video>
+          <video src="./wavelinebackground.mp4" autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover rounded-xl opacity-0 group-hover:opacity-[1] transition-all duration-1000" />
         </div>
         <div className="bg-[#232323] rounded-xl col-span-3 row-span-10 group relative">
-          <video autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover rounded-xl opacity-0 group-hover:opacity-[1] transition-all duration-1000">
-            <source src="./wavelinebackground.mp4" />
-          </video>
+          <video src="./wavelinebackground.mp4" autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover rounded-xl opacity-0 group-hover:opacity-[1] transition-all duration-1000" />
         </div>
         <div className="bg-[#232323] rounded-xl col-span-4 row-span-6 flex flex-col justify-between px-[30px] py-[19px]">
           <h1 className=" text-white font-bold font-[eczar] text-[25px] mb-[10px]">Stack I Use</h1>
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       {/* <Preload /> */}
       <LandingPage />
       <About />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
