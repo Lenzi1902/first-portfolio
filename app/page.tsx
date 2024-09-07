779d5e1148691977e30509dadc162392984728ac
Lenzi1902 <akungithub158@gmail.com>
2024-09-07 12:37:04 +0800

    fixed: width and height

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,37 @@
 "use client";
+import { useEffect, useState } from "react";
 import About from "./component/about/About";
 import Footer from "./component/footer/Footer";
 import LandingPage from "./component/landing-page/LandingPage";
+import Preload from "./component/preload/Preload";
 import Project from "./component/project/Project";
 
 export default function Home() {
+  // const [isLoading, setIsLoading] = useState(true);
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 11500);
+  //   return () => clearTimeout(timeout);
+  // }, []);
   return (
-    <div className="relative min-h-screen max-h-screen overflow-x-hidden">
-      {/* {isLoading?<Preload />: } */}
-      {/* <Preload /> */}
-      <LandingPage />
-      <About />
-      <Project />
-      <Footer />
-    </div>
+    <>
+      {/* {isLoading ? (
+        <Preload />
+      ) : (
+        <div className="relative min-h-screen max-h-screen overflow-x-hidden">
+          <LandingPage />
+          <About />
+          <Project />
+          <Footer />
+        </div>
+      )} */}
+      <div className="relative min-h-screen max-w-screen overflow-x-hidden bg-[#000000]">
+        <LandingPage />
+        <About />
+        <Project />
+        <Footer />
+      </div>
+    </>
   );
 }
