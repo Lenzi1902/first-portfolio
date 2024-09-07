8d777757d568115d7eb15c38d3c4877c98c5b26a
Lenzi1902 <akungithub158@gmail.com>
2024-09-07 18:13:42 +0800

    modified: animation contact button

--- app/component/footer/anim.ts
+++ app/component/footer/anim.ts
@@ -1,9 +1,25 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
 const anim = (element: HTMLElement) => {
-  const tl = gsap.timeline({ defaults: { ease: "power4.inOut", duration: 3 } });
+  gsap.registerPlugin(ScrollTrigger);
+  const tl = gsap.timeline({
+    defaults: {
+      ease: "power4.inOut",
+      duration: 3,
+      scrollTrigger: {
+        trigger: ".bottom-component",
+        start: "top, 90%",
+        end: "top, 80%",
+        scrub: 7,
+      },
+    },
+  });
   gsap.set(".border-bottom-footer", {
     scaleX: 0,
+  });
+  gsap.set(".button-contact-me", {
+    yPercent: 200,
   });
   gsap.set(".button-contact-me", {
     opacity: 0,
@@ -16,7 +32,7 @@ const anim = (element: HTMLElement) => {
       opacity: 1,
       ease: "elastic.inOut(1,0.5)",
       yoyo: true,
-      yPercent: -250,
+      yPercent: -10,
     },
     "-=2"
   );
