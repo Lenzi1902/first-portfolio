65529aaa7abdde3c83ace0722a2f32b0a2807671
Lenzi1902 <akungithub158@gmail.com>
2024-09-07 12:27:53 +0800

    modified: add marquee, videobackground, animation

--- app/component/about/About.tsx
+++ app/component/about/About.tsx
@@ -4,6 +4,8 @@
 import { SkillStack } from "@/app/config/Skills-Stack-config";
 import { useEffect, useRef } from "react";
 import { marqueeSkillStack } from "./anim";
+import VideoBackgroundHover from "./VideoBackgroundHover";
+import MarqueeText from "./MarqueeText";
 
 const About = () => {
   const skillStack = useRef<HTMLDivElement>(null);
@@ -13,46 +15,55 @@ const About = () => {
     }
   }, []);
   return (
-    //! semuanya belum di isi hover about background
-    <div className=" h-screen w-screen text-black px-[119px] py-[52px]">
-      <div className=" h-full w-full grid grid-cols-10 grid-rows-12 gap-4">
-        <div className="bg-[#232323] rounded-xl col-span-3 row-span-8 p-[30px] flex flex-col justify-between relative group ">
-          <video src="./wavelinebackground.mp4" autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover rounded-xl opacity-0 group-hover:opacity-[1] transition-all duration-1000" />
-          <div className=" z-[1]">
-            <img src="./fotoprofileportfolio.svg" alt="" className=" w-[85px] h-[85px]" />
-            <p className=" font-[eczar] text-white font-bold text-[25px]">My name is Agung, I’m a Fullstack Web Developer and living in Indonesia</p>
-          </div>
-        </div>
-        <div className="bg-[#232323] rounded-xl col-span-2 row-span-4 group relative">
-          <video src="./wavelinebackground.mp4" autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover rounded-xl opacity-0 group-hover:opacity-[1] transition-all duration-1000" />
-        </div>
-        <div className="bg-[#232323] rounded-xl col-span-2 row-span-4 group relative">
-          <video src="./wavelinebackground.mp4" autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover rounded-xl opacity-0 group-hover:opacity-[1] transition-all duration-1000" />
-        </div>
-        <div className="bg-[#232323] rounded-xl col-span-3 row-span-10 group relative">
-          <video src="./wavelinebackground.mp4" autoPlay muted loop className="absolute top-0 left-0 w-full h-full object-cover rounded-xl opacity-0 group-hover:opacity-[1] transition-all duration-1000" />
-        </div>
-        <div className="bg-[#232323] rounded-xl col-span-4 row-span-6 flex flex-col justify-between px-[30px] py-[19px]">
-          <h1 className=" text-white font-bold font-[eczar] text-[25px] mb-[10px]">Stack I Use</h1>
-          <div className="w-full h-[100px] relative z-[3] overflow-x-hidden ">
-            <div className=" z-[2] absolute h-full w-[50px] bg-gradient-to-r from-[#232323] from-[27%] to-[rgba(36,34,34,84%)]"></div>
-            <div className="flex h-full gap-[8px] absolute z-[1] container-stack" ref={skillStack}>
-              {SkillStack.map((data, index) => (
-                <div
-                  className="h-full w-[100px] bg-white rounded-xl flex justify-center items-center 
-                skillItem"
-                  key={index}
-                >
-                  <img src={data.sources} alt={data.alt} className=" w-[80%] h-[80%]" />
-                </div>
-              ))}
+    <div className=" min-h-screen w-screen text-black px-[119px] py-[52px]  " ref={skillStack}>
+      <div className=" h-full w-full grid grid-cols-10 grid-rows-17 gap-4">
+        <div className=" rounded-xl col-span-12 row-span-8 relative group bg-[url('/backgroundcolorabout.jpg')]  h-[350px]">
+          <div className=" w-[250px] h-[90px]  absolute top-[43%] z-[2]">
+            <div className=" relative flex items-center w-full h-full  rounded-r-full bg-black">
+              <p className=" text-white w-[60px] ml-5">Located in the Indonesia</p>
+              <div className=" h-full w-[90px]  rounded-full absolute right-0">
+                <video src="./globalanimatin1;1.mp4" suppressHydrationWarning={true} autoPlay muted loop className="  h-[90px] rounded-full" />
+              </div>
             </div>
-            <div className="z-[2] right-0 absolute h-full w-[50px] bg-gradient-to-l from-[#232323] from-[27%] to-[rgba(36,34,34,84%)]"></div>
+          </div>
+          <div className="w-full h-full  flex flex-col justify-between overflow-y-hidden">
+            <VideoBackgroundHover />
+            <div className=" z-[1] h-full w-full relative flex justify-center items-center">
+              <img src="./fotoport2.png" alt="" className=" absolute  bottom-[-30px]  h-[140%] z-[-1] my-image" />
+            </div>
+          </div>
+          <div className="  text-white absolute top-[26%] right-[4%]  h-[150px] w-[340px] flex flex-col justify-between">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="size-7">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 4.5 15 15m0 0V8.25m0 11.25H8.25" />
+            </svg>
+            <div className=" ml-3 text-[30px] font-bold">
+              <h1>Freelance</h1>
+              <h1>Designer & Developer</h1>
+            </div>
           </div>
         </div>
-        <div className="bg-[#232323] rounded-xl col-span-3 row-span-6">6</div>
-        <div className="bg-[#232323] rounded-xl col-span-3 row-span-4">7</div>
-        <div className="bg-[#232323] rounded-xl col-span-4 row-span-4">8</div>
+        <MarqueeText />
+        <div className="rounded-xl col-span-12 row-span-6  relative group ba">
+          <div className="  w-full h-full bg-[url('/backgroundcolorabout.jpg')]  bg-no-repeat rounded-xl flex flex-col justify-between px-[30px] py-[19px] ">
+            <VideoBackgroundHover />
+            <h1 className=" text-white font-bold font-[eczar] text-[25px] mb-[10px] z-10">Stack I Use</h1>
+            <div className="w-full h-[100px] relative z-[3] overflow-x-hidden ">
+              <div className=" z-[2] absolute h-full w-[50px] bg-gradient-to-r left-[-2px] from-[#656565] from-[27%] group-hover:opacity-0 transition-all duration-1000"></div>
+              <div className="flex h-full gap-[8px] absolute z-[1] ">
+                {SkillStack.map((data, index) => (
+                  <div
+                    className="h-full w-[100px] bg-white rounded-xl group-hover:bg-transparent flex justify-center items-center transition-all duration-1000
+                skillItem"
+                    key={index}
+                  >
+                    <img src={data.sources} alt={data.alt} className=" w-[80%] h-[80%]" />
+                  </div>
+                ))}
+              </div>
+              <div className="z-[2] right-0 absolute h-full w-[50px] bg-gradient-to-l from-[#545454] from-[27%] group-hover:opacity-0 transition-all duration-1000"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
