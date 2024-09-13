6be111c887ff35df5b9161c8133d462a37828d9c
Lenzi1902 <akungithub158@gmail.com>
2024-09-13 21:58:41 +0800

    add: Adding LIst data projects

---
+++ app/config/Projects-List-data-config.ts
@@ -0,0 +1,99 @@
+const projectStackListLenziStore = [
+  { sources: "./express.png", alt: "Express Js" },
+  { sources: "./ts.png", alt: "TypeScript" },
+  { sources: "./tailwind.png", alt: "Tailwind" },
+  { sources: "./stripe.webp", alt: "Stripe" },
+  { sources: "./mongodb.png", alt: "MongoDB" },
+  { sources: "./logo vite.png", alt: "Vite" },
+  { sources: "./react.png", alt: "React Js" },
+  { sources: "./reactquery.png", alt: "ReactQuery" },
+  { sources: "./js.png", alt: "Javascript" },
+  { sources: "./shadcn logo.png", alt: "Shadcn" },
+  { sources: "./node-js.png", alt: "Node Js" },
+  { sources: "./css.png", alt: "CSS" },
+  { sources: "./html.png", alt: "HTML" },
+];
+const projectStackListFoodOrdering = [
+  { sources: "./express.png", alt: "Express Js" },
+  { sources: "./ts.png", alt: "TypeScript" },
+  { sources: "./tailwind.png", alt: "Tailwind" },
+  { sources: "./stripe.webp", alt: "Stripe" },
+  { sources: "./mongodb.png", alt: "MongoDB" },
+  { sources: "./logo vite.png", alt: "Vite" },
+  { sources: "./react.png", alt: "React Js" },
+  { sources: "./reactquery.png", alt: "ReactQuery" },
+  { sources: "./js.png", alt: "Javascript" },
+  { sources: "./shadcn logo.png", alt: "Shadcn" },
+  { sources: "./node-js.png", alt: "Node Js" },
+  { sources: "./css.png", alt: "CSS" },
+  { sources: "./html.png", alt: "HTML" },
+];
+const projectStacListkCarsCommers = [
+  { sources: "./express.png", alt: "Express Js" },
+  { sources: "./ts.png", alt: "TypeScript" },
+  { sources: "./tailwind.png", alt: "Tailwind" },
+  { sources: "./stripe.webp", alt: "Stripe" },
+  { sources: "./mongodb.png", alt: "MongoDB" },
+  { sources: "./logo vite.png", alt: "Vite" },
+  { sources: "./react.png", alt: "React Js" },
+  { sources: "./reactquery.png", alt: "ReactQuery" },
+  { sources: "./js.png", alt: "Javascript" },
+  { sources: "./shadcn logo.png", alt: "Shadcn" },
+  { sources: "./node-js.png", alt: "Node Js" },
+  { sources: "./css.png", alt: "CSS" },
+  { sources: "./html.png", alt: "HTML" },
+];
+const projectStackListOrderingApp = [
+  { sources: "./express.png", alt: "Express Js" },
+  { sources: "./ts.png", alt: "TypeScript" },
+  { sources: "./tailwind.png", alt: "Tailwind" },
+  { sources: "./stripe.webp", alt: "Stripe" },
+  { sources: "./mongodb.png", alt: "MongoDB" },
+  { sources: "./logo vite.png", alt: "Vite" },
+  { sources: "./react.png", alt: "React Js" },
+  { sources: "./reactquery.png", alt: "ReactQuery" },
+  { sources: "./js.png", alt: "Javascript" },
+  { sources: "./shadcn logo.png", alt: "Shadcn" },
+  { sources: "./node-js.png", alt: "Node Js" },
+  { sources: "./css.png", alt: "CSS" },
+  { sources: "./html.png", alt: "HTML" },
+];
+
+export const myProjects = [
+  {
+    name: "Lenzi Store",
+    linkGithub: "https://github.com/Lenzi1902/lenzistore-frontend",
+    image: "./lenzistoremockup.png",
+    className: "lenzi-store",
+    description:
+      "Lenzi Store is an e-commerce platform dedicated to buying and selling shoes. On this website, users can browse and purchase a wide variety of shoes, while also having the ability to create their own store. This feature allows anyone to easily set up a virtual storefront and start selling shoes. With a modern design and user-friendly experience, Lenzi Store offers a complete solution for both shoe buyers and sellers in one convenient, secure platform.",
+    projectStack: projectStackListLenziStore,
+  },
+  {
+    name: "Food Ordering",
+    linkGithub: "https://github.com/Lenzi1902/latihan-mern-food-ordering-app-frontend",
+    image: "./foodorderingmockup.png",
+    className: "food-ordering",
+    description:
+      "Food Ordering is an e-commerce platform designed for buying and selling food. Users can browse various food items, place orders, and have their meals delivered directly to their location. Additionally, the platform allows users to create their own food stores, enabling anyone to set up a virtual shop and start selling their culinary creations. With a seamless user experience and efficient delivery system, Food Ordering offers a comprehensive solution for food lovers and entrepreneurs alike.",
+    projectStack: projectStackListFoodOrdering,
+  },
+  {
+    name: "Cars Commers",
+    linkGithub: "https://github.com/Lenzi1902/cars-commers-frontend",
+    image: "./carscommersmockup.png",
+    className: "cars-commers",
+    description:
+      "Cars Commers is an online platform dedicated to buying and selling cars. Users can explore a wide range of vehicles for sale and make purchases directly through the website. Additionally, the platform allows users to create their own dealerships, making it easy for anyone to establish a virtual car showroom and start selling vehicles. With its modern interface and comprehensive features, Cars Commers provides a convenient and secure marketplace for both car buyers and sellers.",
+    projectStack: projectStacListkCarsCommers,
+  },
+  {
+    name: "Ordering app",
+    linkGithub: "https://github.com/Lenzi1902/mern-food-ordering-app-frontend",
+    image: "./Macbooktest-removebg-preview.png",
+    className: "ordering-app",
+    description:
+      "Lorem ipsum odor amet, consectetuer adipiscing elit. Nullam parturient ipsum sit pharetra mollis porta magna morbi. Metus nostra tincidunt convallis nostra venenatis ornare, nostra maximus urna? Mi faucibus efficitur sapien netus donec natoque sem. Cursus ipsum libero vehicula dui sagittis magnis quisque. Sem curae nisl finibus nullam pulvinar enim. Volutpat odio euismod fringilla convallis finibus purus malesuada.",
+    projectStack: projectStackListOrderingApp,
+  },
+];
