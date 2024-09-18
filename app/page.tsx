8106995704bf6d3f3f1b2ef5cf1bfab425341b1f
Lenzi1902 <144054026+Lenzi1902@users.noreply.github.com>
2024-09-18 15:08:24 +0800

    modified: fixed preload

--- app/page.tsx
+++ app/page.tsx
@@ -7,25 +7,24 @@ import Preload from "./component/preload/Preload";
 import Project from "./component/project/Project";
 
 export default function Home() {
-  // const [isLoading, setIsLoading] = useState(true);
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     setIsLoading(false);
-  //   }, 11500);
-  //   return () => clearTimeout(timeout);
-  // }, []);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const hasVisited = sessionStorage.getItem("hasVisited");
+    if (!hasVisited) {
+      const timeout = setTimeout(() => {
+        setIsLoading(false);
+        sessionStorage.setItem("hasVisited", "true");
+      }, 11500);
+      return () => clearTimeout(timeout);
+    } else {
+      setIsLoading(false);
+    }
+  }, []);
+  if (isLoading) {
+    return <Preload />;
+  }
   return (
     <>
-      {/* {isLoading ? (
-        <Preload />
-      ) : (
-        <div className="relative min-h-screen max-h-screen overflow-x-hidden">
-          <LandingPage />
-          <About />
-          <Project />
-          <Footer />
-        </div>
-      )} */}
       <div className="relative min-h-screen max-w-screen overflow-x-hidden bg-[#000000]">
         <LandingPage />
         <About />
