b1b23ccb961e92a2da1c740133076afb72828494
Lenzi1902 <144054026+Lenzi1902@users.noreply.github.com>
2024-09-19 18:12:55 +0800

    modified: fixed error type

--- app/common/GsapMagnetic.tsx
+++ app/common/GsapMagnetic.tsx
@@ -9,15 +9,18 @@ const GsapMagnetic = ({ children }: Props) => {
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const mouseMove = (e) => {
-      const { clientX, clientY } = e;
-      const { width, height, left, top } = ref.current?.getBoundingClientRect();
-      const x = clientX - (left + width / 2);
-      const y = clientY - (top + height / 2);
-      gsap.to(ref.current, { x: x });
-      gsap.to(ref.current, { y: y });
+    const mouseMove = (e: MouseEvent) => {
+      if (ref.current) {
+        const { clientX, clientY } = e;
+        const rect = ref.current.getBoundingClientRect();
+        const x = clientX - (rect.left + rect.width / 2);
+        const y = clientY - (rect.top + rect.height / 2);
+        gsap.to(ref.current, { x });
+        gsap.to(ref.current, { y });
+      }
     };
-    const mouseLeave = (e) => {
+
+    const mouseLeave = (e: MouseEvent) => {
       gsap.to(ref.current, { x: 0 });
       gsap.to(ref.current, { y: 0 });
     };
@@ -30,6 +33,7 @@ const GsapMagnetic = ({ children }: Props) => {
       ref.current?.removeEventListener("mouseleave", mouseLeave);
     };
   }, []);
+
   return <div ref={ref}>{children}</div>;
 };
 
