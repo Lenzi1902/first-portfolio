a642f13a35513fb9603ca9bf9791c7fde25e7a25
Lenzi1902 <144054026+Lenzi1902@users.noreply.github.com>
2025-03-06 20:41:32 +0800

    modified:adding-foto-about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
     <div className=" text-white z-[-1] md:px-[160px] min-h-screen  max-w-screen md:pt-[180px] xs:pt-[100px] bg-[url('/backgroundcolorlandingpage.png')] bg-cover bg-center bg-no-repeat">
       <div className=" flex gap-[2rem] md:text-[1.375rem] text-[#a19f94] font-[eczar]">
         <div className=" relative">
-          <img src="https://res.cloudinary.com/dteuj87u5/image/upload/v1741190024/myFoto_1_h3ri4p.png" alt="MyFoto" className=" md:w-[90rem]" />
+          <img src="./myFoto1.png" alt="MyFoto" className=" md:w-[90rem]" />
           <div className=" h-[5rem] w-[100%] bottom-0 absolute bg-[linear-gradient(to_top,black_30%,transparent_60%)]"></div>
         </div>
         <div className=" flex flex-col gap-[2rem]">
